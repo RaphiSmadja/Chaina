dc4b09cc447de01113c363f2572160f05a25f49e
RaphiSmadja <raphael_smadja@hotmail.com>
2023-12-09 14:19:02 +0100

    first commit exo js

---
+++ exercices/exo1.js
@@ -0,0 +1,11 @@
+function isPair(val) { // val = x // %
+    if (val % 2 == 0) {
+        console.log("Pair")
+    } else {
+        console.log("Impair")
+    }
+}
+
+isPair(10);
+isPair(7);
+isPair(5);
---
+++ exercices/exo2.js
@@ -0,0 +1,6 @@
+function searchOldNumber(val1,vaL2,val3) {  
+    
+}
+
+searchOldNumber(2,5,3)
+searchOldNumber(52,15,13)
